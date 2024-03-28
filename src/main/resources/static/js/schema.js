6bc9f8592526eda5afc1e5d15a0f49fafae6fee3
Rafał Karchier <rafal.karchier@gmail.com>
2024-03-28 14:26:16 +0100

    1-bit-summator add

--- src/main/resources/static/js/schema.js
+++ src/main/resources/static/js/schema.js
@@ -43,9 +43,74 @@ function oneBitSumatorOutput() {
     var b = document.getElementById("b").value;
     var c = document.getElementById("c").value;
 
-    var q = ((a | b) & c) | (a & b);
-    var s = (a ^ b) ^ c;
+    var or1 = a | b;
+    var and1 = a & b;
+    var and2 = or1 & c;
+    var xor1 = a ^ b;
+
+    var q = and2 | and1;
+    var s = xor1 ^ c;
 
     document.getElementById("s").value = 'S: ' + s;
     document.getElementById("q").value = 'C(n+1): ' + q;
+
+    var tableRows = document.getElementById("boardOfTruth").rows;
+    for (const row of tableRows) {
+        row.style.color = "black";
+    }
+
+    switch(a) {
+        case '0':
+            if (b === '0') {
+                if (c === '0') {
+                    tableRows.item(1).style.color = "red";
+                } else {
+                    tableRows.item(2).style.color = "red";
+                }
+            } else {
+                if (c === '0') {
+                    tableRows.item(3).style.color = "red";
+                } else {
+                    tableRows.item(4).style.color = "red";
+                }
+            }
+        break;
+
+        case '1':
+            if (b === '0') {
+                if (c === '0') {
+                    tableRows.item(5).style.color = "red";
+                } else {
+                    tableRows.item(6).style.color = "red";
+                }
+            } else {
+                if (c === '0') {
+                    tableRows.item(7).style.color = "red";
+                } else {
+                    tableRows.item(8).style.color = "red";
+                }
+            }
+        break;
+    }
+
+    var svg = document.getElementById("1-bit-summator-schema").contentDocument;
+
+    svg.getElementById("or1-a").style.stroke = (a == 1 ? "red" : "black");
+    svg.getElementById("or1-b").style.stroke = (b == 1 ? "red" : "black");
+    svg.getElementById("or1-s").style.stroke = (or1 == 1 ? "red" : "black");
+    svg.getElementById("or2-s").style.stroke = (q == 1 ? "red" : "black");
+    svg.getElementById("and1-a").style.stroke = (a == 1 ? "red" : "black");
+    svg.getElementById("and1-dot").style.stroke = (a == 1 ? "red" : "black");
+    svg.getElementById("and1-b").style.stroke = (b == 1 ? "red" : "black");
+    svg.getElementById("and1-s").style.stroke = (and1 == 1 ? "red" : "black");
+    svg.getElementById("and2-b").style.stroke = (c == 1 ? "red" : "black");
+    svg.getElementById("and2-s").style.stroke = (and2 == 1 ? "red" : "black");
+    svg.getElementById("xor1-a").style.stroke = (a == 1 ? "red" : "black");
+    svg.getElementById("xor1-dot").style.stroke = (a == 1 ? "red" : "black");
+    svg.getElementById("xor1-b").style.stroke = (b == 1 ? "red" : "black");
+    svg.getElementById("xor1-dot2").style.stroke = (b == 1 ? "red" : "black");
+    svg.getElementById("xor1-s").style.stroke = (xor1 == 1 ? "red" : "black");
+    svg.getElementById("xor2-b").style.stroke = (c == 1 ? "red" : "black");
+    svg.getElementById("xor2-dot").style.stroke = (c == 1 ? "red" : "black");
+    svg.getElementById("xor2-s").style.stroke = (s == 1 ? "red" : "black");
 }
