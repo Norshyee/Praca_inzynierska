5b85a741abfbf34cdab970f0b97f80c47df409fe
Rafał Karchier <rafal.karchier@gmail.com>
2024-04-01 14:01:47 +0200

    4-bit-serial-sumator

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -19,8 +19,20 @@ function applyCSS(contentArea) {
 }
 
 function randomInputs() {
-    document.getElementById("a").value = (Math.random() >= 0.5) ? 1 : 0;
-    document.getElementById("b").value = (Math.random() >= 0.5) ? 1 : 0;
-    document.getElementById("c").value = (Math.random() >= 0.5) ? 1 : 0;
-    document.getElementById("d").value = (Math.random() >= 0.5) ? 1 : 0;
+    if (document.getElementById("a")) document.getElementById("a").value = (Math.random() >= 0.5) ? 1 : 0;
+    if (document.getElementById("b")) document.getElementById("b").value = (Math.random() >= 0.5) ? 1 : 0;
+    if (document.getElementById("c")) document.getElementById("c").value = (Math.random() >= 0.5) ? 1 : 0;
+    if (document.getElementById("d")) document.getElementById("d").value = (Math.random() >= 0.5) ? 1 : 0;
+
+    if (document.getElementById("a1")) document.getElementById("a1").value = (Math.random() >= 0.5) ? 1 : 0;
+    if (document.getElementById("a2")) document.getElementById("a2").value = (Math.random() >= 0.5) ? 1 : 0;
+    if (document.getElementById("a3")) document.getElementById("a3").value = (Math.random() >= 0.5) ? 1 : 0;
+    if (document.getElementById("a4")) document.getElementById("a4").value = (Math.random() >= 0.5) ? 1 : 0;
+
+    if (document.getElementById("b1")) document.getElementById("b1").value = (Math.random() >= 0.5) ? 1 : 0;
+    if (document.getElementById("b2")) document.getElementById("b2").value = (Math.random() >= 0.5) ? 1 : 0;
+    if (document.getElementById("b3")) document.getElementById("b3").value = (Math.random() >= 0.5) ? 1 : 0;
+    if (document.getElementById("b4")) document.getElementById("b4").value = (Math.random() >= 0.5) ? 1 : 0;
+
+    if (document.getElementById("c1")) document.getElementById("c1").value = (Math.random() >= 0.5) ? 1 : 0;
 }
--- src/main/resources/static/js/schema.js
+++ src/main/resources/static/js/schema.js
@@ -114,3 +114,69 @@ function oneBitSumatorOutput() {
     svg.getElementById("xor2-dot").style.stroke = (c == 1 ? "red" : "black");
     svg.getElementById("xor2-s").style.stroke = (s == 1 ? "red" : "black");
 }
+
+function fourBitSumatorOutput() {
+    var a1 = document.getElementById("a1").value;
+    var a2 = document.getElementById("a2").value;
+    var a3 = document.getElementById("a3").value;
+    var a4 = document.getElementById("a4").value;
+
+    var b1 = document.getElementById("b1").value;
+    var b2 = document.getElementById("b2").value;
+    var b3 = document.getElementById("b3").value;
+    var b4 = document.getElementById("b4").value;
+
+    var c1 = document.getElementById("c1").value;
+
+    var firstAdder = oneBitAdder(a1,b1,c1);
+    var secondAdder = oneBitAdder(a2,b2,firstAdder.carry);
+    var thirdAdder = oneBitAdder(a3,b3,secondAdder.carry);
+    var fourthAdder = oneBitAdder(a4,b4,thirdAdder.carry);
+
+    document.getElementById("s1").value = firstAdder.sum;
+    document.getElementById("c2").value = firstAdder.carry;
+
+    document.getElementById("s2").value = secondAdder.sum;
+    document.getElementById("c3").value = secondAdder.carry;
+
+    document.getElementById("s3").value = thirdAdder.sum;
+    document.getElementById("c4").value = thirdAdder.carry;
+
+    document.getElementById("s4").value = fourthAdder.sum;
+    document.getElementById("c5").value = fourthAdder.carry;
+
+    var add1 = document.getElementById("4-bit-sumator-1").contentDocument;
+    add1.getElementById("a").style.stroke = (a1 == 1 ? "red" : "black");
+    add1.getElementById("b").style.stroke = (b1 == 1 ? "red" : "black");
+    add1.getElementById("c-in").style.stroke = (c1 == 1 ? "red" : "black");
+    add1.getElementById("c-out").style.stroke = (firstAdder.carry == 1 ? "red" : "black");
+    add1.getElementById("sum").style.stroke = (firstAdder.sum == 1 ? "red" : "black");
+
+    var add2 = document.getElementById("4-bit-sumator-2").contentDocument;
+    add2.getElementById("a").style.stroke = (a2 == 1 ? "red" : "black");
+    add2.getElementById("b").style.stroke = (b2 == 1 ? "red" : "black");
+    add2.getElementById("c-in").style.stroke = (firstAdder.carry == 1 ? "red" : "black");
+    add2.getElementById("c-out").style.stroke = (secondAdder.carry == 1 ? "red" : "black");
+    add2.getElementById("sum").style.stroke = (secondAdder.sum == 1 ? "red" : "black");
+
+    var add3 = document.getElementById("4-bit-sumator-3").contentDocument;
+    add3.getElementById("a").style.stroke = (a3 == 1 ? "red" : "black");
+    add3.getElementById("b").style.stroke = (b3 == 1 ? "red" : "black");
+    add3.getElementById("c-in").style.stroke = (secondAdder.carry == 1 ? "red" : "black");
+    add3.getElementById("c-out").style.stroke = (thirdAdder.carry == 1 ? "red" : "black");
+    add3.getElementById("sum").style.stroke = (thirdAdder.sum == 1 ? "red" : "black");
+
+    var add4 = document.getElementById("4-bit-sumator-4").contentDocument;
+    add4.getElementById("a").style.stroke = (a4 == 1 ? "red" : "black");
+    add4.getElementById("b").style.stroke = (b4 == 1 ? "red" : "black");
+    add4.getElementById("c-in").style.stroke = (thirdAdder.carry == 1 ? "red" : "black");
+    add4.getElementById("c-out").style.stroke = (fourthAdder.carry == 1 ? "red" : "black");
+    add4.getElementById("sum").style.stroke = (fourthAdder.sum == 1 ? "red" : "black");
+}
+
+function oneBitAdder(a, b, c) {
+    var sum = (a ^ b) ^ c;
+    var carry = ((a | b) & c) | (a & b);
+
+    return {sum, carry};
+}
