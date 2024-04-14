6643fd93a7bb459f4f40e58c7f39943fc2b60aa4
Rafał Karchier <rafal.karchier@gmail.com>
2024-04-14 11:26:46 +0200

    test

--- src/main/java/pl/ansb/conf/WebConfig.java
+++ src/main/java/pl/ansb/conf/WebConfig.java
@@ -3,6 +3,8 @@ package pl.ansb.conf;
 import org.springframework.boot.autoconfigure.web.ConditionalOnEnabledResourceChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 
@@ -13,5 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
     @ConditionalOnEnabledResourceChain
     public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
         return new ResourceUrlEncodingFilter();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noCache().cachePrivate());
     }
 }
--- src/main/resources/static/js/gate.js
+++ src/main/resources/static/js/gate.js
@@ -25,40 +25,7 @@ function notGateOutput() {
     svg.getElementById("gC").style.stroke = (c == 1 ? "red" : "black");
 }
 
-function andGateOutput() {
-    var a = document.getElementById("a").value;
-    var b = document.getElementById("b").value;
-    var q = a & b;
 
-    document.getElementById("q").value = q;
-
-    var tableRows = document.getElementById("boardOfTruth").rows;
-    for (const row of tableRows) {
-        row.style.color = "black";
-    }
-
-    switch (a) {
-        case '0':
-            if (b === '0')
-                tableRows.item(1).style.color = "red";
-            else
-                tableRows.item(3).style.color = "red";
-            break;
-
-        case '1':
-            if (b === '0')
-                tableRows.item(2).style.color = "red";
-            else
-                tableRows.item(4).style.color = "red";
-            break;
-    }
-
-    var svg = document.getElementById("and-gate").contentDocument;
-
-    svg.getElementById("gA").style.stroke = (a == 1 ? "red" : "black");
-    svg.getElementById("gB").style.stroke = (b == 1 ? "red" : "black");
-    svg.getElementById("gC").style.stroke = (q == 1 ? "red" : "black");
-}
 
 function nandGateOutput() {
     var a = document.getElementById("a").value;
