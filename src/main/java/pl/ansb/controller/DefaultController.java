4d9a10e5e62dfc01cc29b7dcd1174fbf900a537f
Rafał Karchier <rafal.karchier@gmail.com>
2024-04-14 15:04:03 +0200

    add project version with commit id to footbar

--- src/main/java/pl/ansb/controller/DefaultController.java
+++ src/main/java/pl/ansb/controller/DefaultController.java
@@ -1,7 +1,24 @@
 package pl.ansb.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DefaultController {
+
+    @Value("${project.version}")
+    private String projectVersion;
+
+    @Value("${git.commit.id.abbrev}")
+    private String commitId;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("version", projectVersion);
+        model.addAttribute("commitId", commitId);
+
+        return "index";
+    }
 }
