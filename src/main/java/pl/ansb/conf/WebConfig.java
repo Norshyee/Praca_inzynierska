d6d42b3c1162e0b04499fb80ecf066f3bd5076bd
Rafał Karchier <rafal.karchier@gmail.com>
2024-04-14 11:33:27 +0200

    Revert "test"
    This reverts commit 6643fd93a7bb459f4f40e58c7f39943fc2b60aa4.

--- src/main/java/pl/ansb/conf/WebConfig.java
+++ src/main/java/pl/ansb/conf/WebConfig.java
@@ -3,8 +3,6 @@ package pl.ansb.conf;
 import org.springframework.boot.autoconfigure.web.ConditionalOnEnabledResourceChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.CacheControl;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 
@@ -15,12 +13,5 @@ public class WebConfig implements WebMvcConfigurer {
     @ConditionalOnEnabledResourceChain
     public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
         return new ResourceUrlEncodingFilter();
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
-                .setCacheControl(CacheControl.noCache().cachePrivate());
     }
 }
