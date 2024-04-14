3a2d93fd5da08ea06defed4cd3ea35d3dfefead8
Rafał Karchier <rafal.karchier@gmail.com>
2024-04-14 13:12:42 +0200

    optimilize imports

--- src/main/java/pl/ansb/conf/WebConfig.java
+++ src/main/java/pl/ansb/conf/WebConfig.java
@@ -1,10 +1,7 @@
 package pl.ansb.conf;
 
-import org.springframework.boot.autoconfigure.web.ConditionalOnEnabledResourceChain;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
