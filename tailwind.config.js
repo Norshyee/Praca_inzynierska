5ea880076386094daaad5112c7120f5a6f2389f3
Rafał Karchier <rafal.karchier@gmail.com>
2024-04-19 21:52:01 +0200

    Revert "upd5"
    This reverts commit 8318bdfb6fc058e6dc61a478394471b4bb58ef5c.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: process.env.NODE_ENV ? 'jit' : undefined,
   content: ["./src/**/*.{html,js}"],
-  darkMode: false,
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
