8318bdfb6fc058e6dc61a478394471b4bb58ef5c
Rafal <rafal.karchier@gmail.com>
2024-04-19 20:01:13 +0200

    upd5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: process.env.NODE_ENV ? 'jit' : undefined,
   content: ["./src/**/*.{html,js}"],
+  darkMode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
